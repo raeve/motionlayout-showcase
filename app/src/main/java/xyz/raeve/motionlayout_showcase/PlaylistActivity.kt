b1bb64e0a29215e9a9a412c7406509b3cdbe6f60
Rubén Expósito <ruben.exposito@apiumhub.com>
2020-02-06 11:49:58 +0100

    Add playlist spotify scene

--- app/src/main/java/xyz/raeve/motionlayout_showcase/PlaylistActivity.kt
+++ app/src/main/java/xyz/raeve/motionlayout_showcase/PlaylistActivity.kt
@@ -2,10 +2,39 @@ package xyz.raeve.motionlayout_showcase
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_playlist.*
 
 class PlaylistActivity : AppCompatActivity(R.layout.activity_playlist) {
-    companion object {
-        fun getCallingIntent(context: Context?) = Intent(context, PlaylistActivity::class.java)
+  companion object {
+    fun getCallingIntent(context: Context?) = Intent(context, PlaylistActivity::class.java)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    recyclerView.apply {
+      layoutManager = LinearLayoutManager(context)
+      adapter = PlaylistAdapter()
     }
+  }
+}
+
+class PlaylistAdapter : RecyclerView.Adapter<PlaylistAdapter.SongViewHolder>() {
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder =
+    SongViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_song, parent, false))
+
+  override fun getItemCount(): Int = 20
+
+  override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+
+  }
+
+  inner class SongViewHolder(view: View) : RecyclerView.ViewHolder(view)
 }
